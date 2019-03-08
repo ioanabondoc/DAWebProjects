8f0f1ff1725528f93388ca2d3ddabdb54925d15d
Ioana Bondoc <ioana.bondoc17@gmail.com>
2019-03-08 12:28:33 +0200

    initial project structure

---
+++ src/CityStructure/CityStructure/Comercial.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CityStructure
+{
+    class Comercial
+    {
+        //malls,markets,supermarkets,abc,clothing store,
+        //squares(piete)
+        public Mall[] malls;
+        public Shop[] shops;
+        public ComercialSquare[] comercialSquares;
+    }
+    public class ComercialSquare : Location
+    {
+        public string Type { get; set; } //fruits,clothes,cars,etc;
+        public double Area { get; set; }
+    }
+    public class Shop:Location
+    {
+        public string Type { get; set; }//abc,market,clothing store
+    }
+    public class Mall : Location
+    {
+        public Shop[] shops;
+    }
+}
---
+++ src/CityStructure/CityStructure/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CityStructure
+{
+    
+
+    public class City
+    {
+        
+    }
+
+    static class Program
+    {
+    
+        static void Main(string[] args)
+        {
+        }
+    }
+}
