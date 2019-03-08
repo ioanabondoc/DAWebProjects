d32530cb2ac7d895e1678b585eb0beabc45b6d38
Ioana Bondoc <ioana.bondoc17@gmail.com>
2019-03-08 09:28:33 +0200

    Homework1

---
+++ CityStructure/CityStructure/HumanBuildings.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CityStructure
+{
+    public class Address
+    {
+        string StreetName;
+        int[] StreetNr;
+    }
+
+    public class HumanBuildings
+    {
+        public PermanentLivingPlace[] permanentLivingPlaces;
+        public TemporaryLivingPlace[] TemporaryLivingPlaces;
+    }
+
+    public class Location
+    {
+        public string Name { get; set; }
+        public Address Address { get; set; }
+    }
+
+    public class LivingPlace : Location
+    {
+        public int HumanCapacity { get; set; }
+
+    }
+    public class TemporaryLivingPlace : LivingPlace
+    {
+        public string Type { get; set; }
+        public int NrOfRooms { get; set; }
+        /*types: hostel,hotel,pension,etc.*/
+    }
+    public class PermanentLivingPlace : LivingPlace
+    {
+        public string Type { get; set; }
+        public int NrOfApartements { get; set; } //house=1;blocks=plentier
+    }
+}
