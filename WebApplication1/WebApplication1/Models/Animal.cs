d029692c6382b9fea5a865d12407abdf32b0505b
Ioana Bondoc <ioana.bondoc17@gmail.com>
2019-03-16 16:59:31 +0200

    Second homework - a web application (My Zoo)

---
+++ WebApplication1/WebApplication1/Models/Animal.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Animal
+    {
+        public Guid ID { get; set; }
+        public string Name { get; set; }
+        public int Weight { get; set; }
+        public string Type { get; set; }
+        public Animal(string nm,int w, string tp="Terrestrial")
+        {
+            ID = new Guid();
+            Name = nm;
+            Weight = w;
+            Type = tp;
+        }
+        
+    }
+}
